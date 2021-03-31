2e0db38387adb8ade0a6c68c89d7cf042721ac65
IceD0G <34693762+IceD0G@users.noreply.github.com>
2021-03-31 20:59:40 +0300

    Update Song.js

--- src/components/Song.js
+++ src/components/Song.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 
 const Song = ({ currentSong, isPlaying }) => {
+	const imgRef = useRef(null);
 	let play = {};
 	if (!isPlaying) {
 		play = {
@@ -11,6 +12,14 @@ const Song = ({ currentSong, isPlaying }) => {
 			animationPlayState: 'running',
 		};
 	}
+	//useEffect
+	// useEffect(() => {
+	// 	if (isPlaying) {
+	// 		imgRef.current.classList.add('animate');
+	// 	} else {
+	// 		imgRef.current.classList.remove('animate');
+	// 	}
+	// }, [isPlaying]);
 	return (
 		<div className='song-container'>
 			<img
@@ -18,6 +27,7 @@ const Song = ({ currentSong, isPlaying }) => {
 				src={currentSong.cover}
 				className='animate'
 				style={play}
+				ref={imgRef}
 			/>
 			<h2>{currentSong.name}</h2>
 			<h3>{currentSong.artist}</h3>
